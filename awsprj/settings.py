af3767c9b1139e67d4784a393bf4a352e3d334db
seun0123 <seunpark04@gmail.com>
2022-11-22 20:55:06 +0900

    siblanom

--- awsprj/settings.py
+++ awsprj/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-8ymi#!6-*tgsjz4+(xkpp%0y&!l1&@8*p-&a%)*^jrx_sln8+w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".ap-northeast-2.compute.amazonaws.com" 
+]
 
 
 # Application definition
